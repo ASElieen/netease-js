d348a0af86f5d84e95ecccac35497f9610c89fb8
username <790768275@qq.com>
2022-09-24 10:32:48 +0800

    完成左右滑动 sessionStorage本地存储分类

--- src/application/Singer/Singers.jsx
+++ src/application/Singer/Singers.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import { NavContainer } from './SingerStyle'
-import HorizenItem from '../../baseUI/Horizen-item/HorizenItem'
-import { categoryTypes,alphaTypes } from '../../api/mock'
+import React, { useState } from "react";
+import { NavContainer } from "./SingerStyle";
+import HorizenItem from "../../baseUI/Horizen-item/HorizenItem";
+import { categoryTypes, alphaTypes } from "../../api/mock";
 
 const Singer = () => {
+  const [categoryName, setCategoryName] = useState("");
+  const [alpha, setAlpha] = useState("");
+
+  let handleAlpha = (val) => {
+    setAlpha(val);
+    sessionStorage.setItem("alpha", val);
+  };
+
+  let handleCategoryName = (val) => {
+    setCategoryName(val);
+    sessionStorage.setItem("category", val);
+  };
   return (
     <NavContainer>
-      <HorizenItem list={categoryTypes} title={"分类(默认热门)"} />
-      <HorizenItem list={alphaTypes} />
+      <HorizenItem
+        list={categoryTypes}
+        title={"分类(默认热门)"}
+        oldVal={sessionStorage.getItem("category") || categoryName}
+        handleClick={(val) => handleCategoryName(val)}
+      />
+      <HorizenItem
+        list={alphaTypes}
+        oldVal={sessionStorage.getItem("alpha") || alpha}
+        handleClick={(val) => handleAlpha(val)}
+      />
     </NavContainer>
   );
-}
+};
 
 export default React.memo(Singer);
