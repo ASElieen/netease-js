455e52115629581d4b6638469bb605c3420353f0
username <790768275@qq.com>
2022-09-26 11:34:12 +0800

    完成滚动时对头部图片的效果处理

--- src/application/SingerInfo/SingerInfo.jsx
+++ src/application/SingerInfo/SingerInfo.jsx
@@ -30,6 +30,7 @@ const SingerInfo = (props) => {
 
   //往上偏移 露出圆角
   const OFFSET =5
+  const HEADER_HEIGHT = 45
 
   useEffect(()=>{
     let h = imageWrapper.current.offsetHeight
@@ -44,6 +45,48 @@ const SingerInfo = (props) => {
   const setShowStatusFalse = useCallback(()=>{
     setShowStatus(false)
   },[])
+
+  //滚动效果的处理
+  const handleScroll = (pos) => {
+    let height = initialHeight.current;
+    const newY = pos.y;
+    const imageDOM = imageWrapper.current;
+    const buttonDOM = collectButton.current;
+    const headerDOM = header.current;
+    const layerDOM = layer.current;
+    const minScrollY = -(height - OFFSET) + HEADER_HEIGHT;
+
+    //滑动距离占图片高度的百分比
+    const percent = Math.abs(newY / height);
+
+    //单纯下拉
+    if (newY > 0) {
+      imageDOM.style["transform"] = `scale (${1 + percent})`;
+      buttonDOM.style["transform"] = `translate3d (0, ${newY}px, 0)`;
+      layerDOM.style.top = `${height - OFFSET + newY}px`;
+    } else if (newY >= minScrollY) {
+      //往上滑动，但是遮罩还没超过 Header 部分
+      layerDOM.style.top = `${height - OFFSET - Math.abs(newY)}px`;
+      // 这时候保证遮罩的层叠优先级比图片高，不至于被图片挡住
+      layerDOM.style.zIndex = 1;
+      imageDOM.style.paddingTop = "75%";
+      imageDOM.style.height = 0;
+      imageDOM.style.zIndex = -1;
+      // 按钮跟着移动且渐渐变透明
+      buttonDOM.style["transform"] = `translate3d (0, ${newY}px, 0)`;
+      buttonDOM.style["opacity"] = `${1 - percent * 2}`;
+    } else if (newY < minScrollY) {
+      //往上滑动，但是遮罩超过 Header 部分
+      layerDOM.style.top = `${HEADER_HEIGHT - OFFSET}px`;
+      layerDOM.style.zIndex = 1;
+      // 防止溢出的歌单内容遮住 Header
+      headerDOM.style.zIndex = 100;
+      // 此时图片高度与 Header 一致
+      imageDOM.style.height = `${HEADER_HEIGHT}px`;
+      imageDOM.style.paddingTop = 0;
+      imageDOM.style.zIndex = 99;
+    }
+  };
 
   return (
     <CSSTransition
@@ -67,7 +110,7 @@ const SingerInfo = (props) => {
         <BgLayer ref={layer}></BgLayer>
 
         <SongListWrapper ref={songScrollWrapper}>
-          <Scroll ref={songScroll}>
+          <Scroll ref={songScroll} onScroll={handleScroll}>
             <SongList songs={artist.hotSongs} showCollect={false}></SongList>
           </Scroll>
         </SongListWrapper>
