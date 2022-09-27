ea9f1564459519133a5e90c16b009a914263de07
username <790768275@qq.com>
2022-09-27 10:32:39 +0800

    完成全屏播放器样式 添加切换动画

--- src/application/Player/MiniPlayer/MiniPlayer.jsx
+++ src/application/Player/MiniPlayer/MiniPlayer.jsx
@@ -1,37 +1,53 @@
-import React from 'react'
+import React,{useRef} from 'react'
+import {CSSTransition} from 'react-transition-group'
 import {AiOutlinePause} from 'react-icons/ai'
 import { BsFillPlayFill, BsMusicNoteList } from "react-icons/bs";
 import { getName } from '../../../api/utils';
 import { MiniPlayerContainer } from './miniPlayerStyle';
+import {useDispatch} from 'react-redux'
 
 const MiniPlayer = (props) => {
-    const { song } = props;
+    const { song,fullScreen,changeFullScreen } = props;
+    const miniPlayerRef = useRef()
+    const dispatch = useDispatch()
   return (
-    <MiniPlayerContainer>
-      <div className="icon">
-        <div className="imgWrapper">
-          <img
-            className="play"
-            src={song.al.picUrl}
-            width="40"
-            height="40"
-            alt="img"
-          />
+    <CSSTransition
+    in={!fullScreen}
+    timeout={400}
+    classNames='mini'
+    onEnter={()=>{
+      miniPlayerRef.current.style.display = 'flex'
+    }}
+    onExited={()=>{
+      miniPlayerRef.current.style.display = 'none'
+    }}
+    >
+      <MiniPlayerContainer ref={miniPlayerRef} onClick={()=>{dispatch(changeFullScreen(true))}}>
+        <div className="icon">
+          <div className="imgWrapper">
+            <img
+              className="play"
+              src={song.al.picUrl}
+              width="40"
+              height="40"
+              alt="img"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="text">
-        <h2 className="name">{song.name}</h2>
-        <p className="desc">{getName(song.ar)}</p>
-      </div>
+        <div className="text">
+          <h2 className="name">{song.name}</h2>
+          <p className="desc">{getName(song.ar)}</p>
+        </div>
 
-      <div className="control">
-        <AiOutlinePause className="iconfont" />
-      </div>
-      <div className="control">
-        <BsMusicNoteList className="iconfont" />
-      </div>
-    </MiniPlayerContainer>
+        <div className="control">
+          <AiOutlinePause className="iconfont" />
+        </div>
+        <div className="control">
+          <BsMusicNoteList className="iconfont" />
+        </div>
+      </MiniPlayerContainer>
+    </CSSTransition>
   );
 }
 
--- src/application/Player/Player.jsx
+++ src/application/Player/Player.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
-import MiniPlayer from './MiniPlayer/MiniPlayer'
-const Player = (props) => {
-    const currentSong = {
-      al: {
-        picUrl:
-          "https://p1.music.126.net/JL_id1CFwNJpzgrXwemh4Q==/109951164172892390.jpg",
-      },
-      name: "木偶人",
-      ar: [{ name: "薛之谦" }],
-    };
-  return (
-    <MiniPlayer song={currentSong}></MiniPlayer>
-  )
-}
+import React from "react";
+import { changeFullScreen } from "../../store/Slices/playerSlice";
+import MiniPlayer from "./MiniPlayer/MiniPlayer";
+import NormalPlayer from "./NormalPlayer/NormalPlayer";
+import { useDispatch, useSelector } from "react-redux";
+const Player = () => {
+  const dispatch = useDispatch();
+  const {
+    fullScreen,
+    playing,
+    currentIndex,
+    playList,
+    mode,
+    sequencePlayList,
+  } = useSelector((store) => store.player);
 
-export default Player
+  const currentSong = {
+    al: {
+      picUrl:
+        "https://p1.music.126.net/JL_id1CFwNJpzgrXwemh4Q==/109951164172892390.jpg",
+    },
+    name: "木偶人",
+    ar: [{ name: "薛之谦" }],
+  };
+  return (
+    <>
+      <MiniPlayer
+        song={currentSong}
+        fullScreen={fullScreen}
+        changeFullScreen={changeFullScreen}
+      ></MiniPlayer>
+      <NormalPlayer
+        song={currentSong}
+        fullScreen={fullScreen}
+        changeFullScreen={changeFullScreen}
+      ></NormalPlayer>
+    </>
+  );
+};
+
+export default Player;
